6e67f987fc5c6d40b1add290174c49da8c591ac4
viktoriaruckhofer <57627832+viktoriaruckhofer@users.noreply.github.com>
2020-12-03 11:57:30 +0100

    Added tool to calculate BMI

--- src/inf01_fitnesstracker/INF01_Fitnesstracker.java
+++ src/inf01_fitnesstracker/INF01_Fitnesstracker.java
@@ -5,6 +5,8 @@
  */
 package inf01_fitnesstracker;
 
+import java.util.Scanner;
+
 /**
  *
  * @author marle
@@ -15,14 +17,45 @@ public class INF01_Fitnesstracker {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        
-        System.out.println("Test");
-        
-        System.out.println("Test2");
-        
-        System.out.println("Test3");
+        Scanner myScanner = new Scanner(System.in);
+         
+        System.out.println("Willkommen beim BMI Rechner");
+        System.out.println("Bitte gib dein Gewicht in KG ein:");
+        double gewicht = myScanner.nextDouble();
+        System.out.println("Bitte gib deine Größe in Meter an (Bsp 1,80):");
+        double groesse = myScanner.nextDouble();
+         
+        double bmi = bmiRechner(groesse, gewicht);
+        ausgabe(bmi);
     }
-    
+    //Ausgabe des BMI und Auswahl des passenden Namen.
+    private static void ausgabe(double bmi) {       
+        System.out.print("Dein BMI beträgt: " + bmi + " ");
+        if(bmi <16) {
+            System.out.println("Starkes Untergewicht!");
+        }else if(bmi>=16 && bmi<17) {
+            System.out.println("Mäßiges Untergewicht.");
+        }else if(bmi>=17 && bmi<18.5) {
+            System.out.println("Leichtes Untergewicht.");
+        }else if(bmi>=18.5 && bmi<25) {
+            System.out.println("Normalgewicht!");
+        }else if(bmi>=25 && bmi<30) {
+            System.out.println("Präadipositas.");
+        }else if(bmi>=30 && bmi<35) {
+            System.out.println("Adipositas Grad I.");
+        }else if(bmi>=35 && bmi<40) {
+            System.out.println("Adipositas Grad II");
+        }else if(bmi>= 40) {
+            System.out.println("Adipositas Grad III");
+        }else {
+            System.out.println("Bitte überprüfe deine Eingabe!");
+        }
 }
+ 
+    private static double bmiRechner(double groesse, double gewicht) {
+        //BMI ausrechnen kg/m**2
+        double bmi = gewicht / (groesse*groesse);
+        //BMI auf 2 Nachkommastellen runden.
+        double d = Math.pow(10, 2);
+        return Math.round(bmi * d) / d;
+    }}
